4eeb5316ee672e108fef437eb6af4c9d8768e3d2
prince owire <princeowire131@gmail.com>
2024-03-11 13:10:58 +0100

    still working

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,11 @@
+import  './home.css'
+import Header from "../../component/header/header";
+function Home() {
+    return (
+        <div className="home-main">
+                <Header />
+        </div>
+    )
+}
+
+export default Home;
